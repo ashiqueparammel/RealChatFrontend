0e89834d9a1f931804b960703d633e003cd9fefc
ashiqueparammel <ashiqueparammel558@gmail.com>
2024-02-14 19:02:40 +0530

    updated home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect } from 'react'
 import toast, { Toaster } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
@@ -10,12 +12,39 @@ function Home() {
   //     navigate('/login')
   //   }
   // }, [token])
-  
+
   return (
     <div>
+      <div class="py-10 h-screen bg-[#000000] px-2 ">
+        <div class="max-w-md mx-auto bg-transparent shadow-lg rounded-lg   md:max-w-lg">
+          <div class="md:flex">
+            <div class="w-full p-4">
 
-      Home
-      <h1 className='font-medium text-red-50 font-roboto-mono  bg-black'> hello</h1>
+              <div class="relative text-white hover:text-black"> <input type="text" class="border w-full h-12 rounded-md bg-transparent   border-[#FAFAFA] focus:border-white outline-none  hover:bg-white pl-6 " placeholder="Search..." />  <FontAwesomeIcon className=' absolute right-3 top-4' icon={faSearch} /> </div>
+              <div className='overflow-y-auto max-h-screen hidescroll'>
+
+                <ul className=''>
+
+                  <li class="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded cursor-pointer transition">
+                    <div class="flex ml-2"> <img src="https://i.imgur.com/aq39RMA.jpg" width="40" height="40" class="rounded-full" />
+                      <div class="flex flex-col ml-2"> <span class="font-medium text-black">Jessica Koel</span> <span class="text-sm text-gray-400 truncate w-32">Hey, Joel, I here to help you out please tell me</span> </div>
+                    </div>
+                    <div class="flex flex-col items-center"> <span class="text-gray-300">11:26</span> <i class="fa fa-star text-green-400"></i> </div>
+                  </li>
+                  <li class="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded cursor-pointer transition">
+                    <div class="flex ml-2"> <img src="https://i.imgur.com/eMaYwXn.jpg" width="40" height="40" class="rounded-full" />
+                      <div class="flex flex-col ml-2"> <span class="font-medium text-black">Komeial Bolger</span> <span class="text-sm text-gray-400 truncate w-32">I will send you all documents as soon as possible</span> </div>
+                    </div>
+                    <div class="flex flex-col items-center"> <span class="text-gray-300">12:26</span> <i class="fa fa-star text-green-400"></i> </div>
+                  </li>
+                  
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
       <Toaster />
     </div>
   )
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -107,7 +107,7 @@ function Login() {
     }
 
     return (
-        <div className='bg-[#000000] w-full h-[695px] flex justify-center '>
+        <div className='bg-[#000000]  w-full h-[695px] flex justify-center '>
             <Card className='bg-transparent  h-[60%] 2xl:w-[35%] xl:w-[40%] lg:w-[40%] md:w-[50%] 2xl:mt-40 xl:mt-28 lg:mt-24 md:mt-20 sm:mt-14 mt-10 w-[90%] sm:w-[80%] flex flex-col justify-center shadow-none items-center rounded-lg gap-5'>
                 <Typography className='font-prompt text-[#FAFAFA] text-3xl 2xl:absolute 2xl:-top-5'>LOGIN</Typography>
                 <input onChange={(e) => setEmail(e.target.value)}
@@ -123,11 +123,11 @@ function Login() {
                     placeholder="Enter Your Password"
                 />
                 <Button onClick={loginHandle} className='font-prompt-normal bg-transparent 2xl:text-2xl xl:text-2xl lg:text-xl md:text-lg sm:text-lg text-md text-white shadow-white shadow-none w-[80%] h-[14%] border-[#FAFAFA] border-[1px] hover:border-[1px]' > LOGIN </Button>
-                <Button onClick={() => LoginWithGoogleAuth()} className='flex items-center text-white shadow-white shadow-none w-[80%] h-[14%] border-[#FAFAFA] border-[1px] hover:border-[1px] hover:shadow-2xl '>
+                <Button onClick={() => LoginWithGoogleAuth()} className=' flex items-center text-white bg-transparent shadow-white shadow-none w-[80%] h-[14%] border-[#FAFAFA] border-[1px] hover:border-[1px] hover:shadow-2xl '>
                     <span>
-                        <FontAwesomeIcon className='bg-white rounded-md text-black p-1 bgcolors 2xl:w-6 2xl:h-6 2xl:mr-5 xl:w-6 xl:h-6 xl:mr-4 lg:w-5 lg:h-5 lg:mr-2 md:w-4 md:h-4 mr-3' icon={faGoogle} />
+                        <FontAwesomeIcon className='bg-white  rounded-md text-black p-1 bgcolors 2xl:w-6 2xl:h-6 2xl:mr-5 xl:w-6 xl:h-6 xl:mr-4 lg:w-5 lg:h-5 lg:mr-2 md:w-4 md:h-4 mr-3' icon={faGoogle} />
                     </span>
-                    <span className='font-prompt-normal bg-transparent 2xl:text-2xl xl:text-2xl lg:text-xl md:text-lg sm:text-lg text-md'>
+                    <span className=' font-prompt-normal bg-transparent 2xl:text-2xl xl:text-2xl lg:text-xl md:text-lg sm:text-lg text-md'>
                         SIGN IN WITH GOOGLE
                     </span>
                 </Button>
